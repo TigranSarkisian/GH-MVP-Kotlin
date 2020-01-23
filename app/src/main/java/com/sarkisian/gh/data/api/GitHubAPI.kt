3e7daab088c08ed756a91f36b0489c54a764a020
Tigran Sarkisian <1731046@gmail.com>
2020-01-23 13:31:24 +0300

    Refactored AuthInterceptor

--- app/src/main/java/com/sarkisian/gh/data/api/GitHubAPI.kt
+++ app/src/main/java/com/sarkisian/gh/data/api/GitHubAPI.kt
@@ -12,23 +12,17 @@ interface GitHubAPI {
 
     @GET("users/{username}/repos")
     fun loadRepos(
-        @Path("username") username: String,
-        @Query("client_id") client_id: String = "5301f7f2e5e82214454b",
-        @Query("client_secret") client_secret: String = "00b3dc2b7c8f9641d71c569a961acbbddf497f8d"
+        @Path("username") username: String
     ): Single<MutableList<Repo>>
 
     @GET("repos/{username}/{repo_name}")
     fun loadRepo(
         @Path("username") username: String,
-        @Path("repo_name") repoName: String,
-        @Query("client_id") client_id: String = "5301f7f2e5e82214454b",
-        @Query("client_secret") client_secret: String = "00b3dc2b7c8f9641d71c569a961acbbddf497f8d"
+        @Path("repo_name") repoName: String
     ): Single<Repo>
 
     @GET("search/repositories")
     fun searchRepos(
-        @Query("client_id") client_id: String = "5301f7f2e5e82214454b",
-        @Query("client_secret") client_secret: String = "00b3dc2b7c8f9641d71c569a961acbbddf497f8d",
         @Query("q") query: String
     ): Single<SearchRequest>
 
